7343df2fcf0ea96b0364b206cd13eae15dfcccae
Moritz Bützer <moritz@buetzer.bz>
2020-06-18 16:01:29 +0200

    config

--- custom_components/vzug/__init__.py
+++ custom_components/vzug/__init__.py
@@ -9,9 +9,9 @@ from homeassistant.components.sensor import PLATFORM_SCHEMA
 import voluptuous as vol
 
 from homeassistant.const import (
-    CONF_PASSWORD,
+    CONF_HOST,
     CONF_USERNAME,
-    CONF_IP_ADDRESS
+    CONF_PASSWORD
 )
 
 DOMAIN = "vzug"
@@ -22,7 +22,7 @@ _LOGGER = logging.getLogger(__name__)
 
 # Validation of the user's configuration
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_IP_ADDRESS): cv.string,
+    vol.Required(CONF_HOST): cv.string,
     vol.Required(CONF_USERNAME): cv.string,
     vol.Required(CONF_PASSWORD): cv.string,
 })
@@ -30,11 +30,13 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 async def async_setup(hass, config):
     hass.states.set('vzug.initializes', True)
 
-    ip_address = config[CONF_IP_ADDRESS]
+    host = config[CONF_HOST]
     username = config[CONF_USERNAME]
-    password = config.get(CONF_PASSWORD)
+    password = config[CONF_PASSWORD]
 
-    async with VZUG(ip_address, username, password) as vzug:
+    _LOGGER.debug("conf %s", config)
+
+    async with VZUG(host, username, password) as vzug:
         # Collect the data of the current state
         await vzug.get_device_status()
         _LOGGER.debug("Device details %s", vzug.device_status)
