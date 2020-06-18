51324967ef4ca05b3c205f5e12f8de82d3721916
Moritz Bützer <moritz@buetzer.bz>
2020-06-18 15:12:17 +0200

    maybe

--- custom_components/vzug/__init__.py
+++ custom_components/vzug/__init__.py
@@ -1,20 +1,40 @@
 """
 The "vzug" custom component.
-This component implements the bare minimum that a component should implement.
-Configuration:
-To use the hello_word component you will need to add the following to your
-configuration.yaml file.
-hello_world:
 """
+import asyncio
+import logging
 
-# The domain of your component. Should be equal to the name of your component.
+import homeassistant.helpers.config_validation as cv
+import voluptuous as vol
+
+from vzug.vzug import VZUG
+
+_LOGGER = logging.getLogger(__name__
+
 DOMAIN = "vzug"
 
+# Validation of the user's configuration
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_IP_ADDRESS): cv.string,
+    vol.Required(CONF_USERNAME): cv.string,
+    vol.Required(CONF_PASSWORD): cv.string,
+})
+
+IP_ADDRESS = "YOUR_IP"
+USERNAME = "YOUR_USERNAME"
+PASSWORD = "YOUR_PASSWORD"
 
 def setup(hass, config):
-    """Set up a skeleton component."""
-    # States are in the format DOMAIN.OBJECT_ID.
     hass.states.set('vzug.Hello_World', 'Works also!')
+
+    ip_address = config[CONF_IP_ADDRESS]
+    username = config[CONF_USERNAME]
+    password = config.get(CONF_PASSWORD)
+
+    async with VZUG(ip_address, username, password) as vzug:
+        # Collect the data of the current state
+        await vzug.get_device_status()
+        _LOGGER.debug("Device details %s", vzug.device_status)
 
     # Return boolean to indicate that initialization was successfully.
     return True
